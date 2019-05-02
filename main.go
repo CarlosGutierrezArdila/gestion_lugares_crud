b28473b2f14a09c889463bdf8c8032ab2044d90e
CarlosGutierrezArdila <gutierrezcarlos14@gmail.com>
2019-05-02 15:48:45 -0500

    api generada por beego ajustada a linemaientos oas

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	_ "oas_sudamericana/gestion_lugares_crud/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+	"github.com/udistrital/utils_oas/customerror"
+)
+
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	//logs
+	logs.SetLogger(logs.AdapterFile, `{"filename":"/var/log/beego/gestion_lugares_crud/gestion_lugares_crud.log"}`)
+	//errores
+	beego.ErrorController(&customerror.CustomErrorController{})
+	beego.Run()
+}
